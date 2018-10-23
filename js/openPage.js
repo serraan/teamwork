815c4b40ab05dd6f3199cb38ddf0f42c82e784f9
Eyegabry99 <eyegabry99@gmail.com>
2018-10-23 14:37:04 +0200

    merge totale

---
+++ js/openPage.js
@@ -0,0 +1,12 @@
+function openPageRic(){
+    location.href="ricerca.html";
+
+}
+
+function openPageDet(){
+    location.href="dettagli.html"
+}
+
+function goBack(){
+    location.href="listafile.html"
+}
